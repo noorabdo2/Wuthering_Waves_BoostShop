837d139335960dcfdcb7cc99075670e316a8a048
Nouralddin <43034992+noorabdo2@users.noreply.github.com>
2024-07-30 12:54:28 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navToggle = document.getElementById("nav-toggle");
   const navMenu = document.getElementById("nav-menu");
-  const cardHeadContents = document.querySelectorAll(".card__head-content");
 
   navToggle.addEventListener("click", function () {
     navMenu.classList.toggle("show");
   });
 });
+
+function showContactPopup() {
+  document.getElementById("contact-popup").style.display = "flex";
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideContactPopup() {
+  document.getElementById("contact-popup").style.display = "none";
+  document.getElementById("overlay").style.display = "none";
+}
